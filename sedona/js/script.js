68ad8c2b84b63d7840a469bea37c743f02e1cde3
Veronika <v.b.171017@gmail.com>
2017-10-06 19:11:53 +0300

    map

--- sedona/js/script.js
+++ sedona/js/script.js
@@ -1,24 +1,19 @@
-// map
-function initialize() {
-  var mapOptions = {
-    zoom: 15,
-    scrollwheel: false,
-    center: new google.maps.LatLng(34.8724375,-111.760678)
-  }
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-    mapOptions);
-  // var image = "../img/company.png";
-  var myLatLng = new google.maps.LatLng(34.8661349,-111.7578885);
-  var beachMarker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    // icon: image
-  });
-}
-google.maps.event.addDomListener(window, 'load', initialize);
-
 $(function() {
 
+// map
+var map;
+
+function initMap() {
+  var coordinates = {lat: 34.8661349, lng: -111.7578885},
+  zoom = 15,
+
+
+
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
+    center: coordinates ={lat: 34.8724375,lng: -111.760678},
+    scrollwheel: false,
+  });
+}
 
 // datepicker
 
