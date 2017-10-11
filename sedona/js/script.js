9a65aaebcbb5dd9091a955504ee312359430aaa5
Veronika <v.b.171017@gmail.com>
2017-10-11 22:08:19 +0300

    upgrade

--- sedona/js/script.js
+++ sedona/js/script.js
@@ -1,7 +1,4 @@
-$(function() {
-
 // map
-
 function initialize() {
   var mapOptions = {
     zoom: 15,
@@ -9,7 +6,7 @@ function initialize() {
     center: new google.maps.LatLng(34.8724375,-111.760678)
   }
   var map = new google.maps.Map(document.getElementById('map-canvas'),
-    mapOptions);
+                                mapOptions);
   // var image = "../img/company.png";
   var myLatLng = new google.maps.LatLng(34.8661349,-111.7578885);
   var beachMarker = new google.maps.Marker({
@@ -21,40 +18,42 @@ function initialize() {
 google.maps.event.addDomListener(window, 'load', initialize);
 
 // datepicker
-
-$('#checkout' ).datepicker(
-  { dateFormat: 'd MM yy' }
-  );
-$('#checkin' ).datepicker(
-  { dateFormat: 'd MM yy' }
-  );
+$(document).ready(function() {
+    $( '#checkout' ).datepicker(
+      { dateFormat: 'd MM yy' }
+      );
+    $( '#checkin' ).datepicker(
+      { dateFormat: 'd MM yy' }
+      );
+  });
 
 // popup
-$('.magnific-popup').magnificPopup({
-  type:'inline',
-  midClick: true,
-}
-);
-
+$(document).ready(function() {
+  $('.magnific-popup').magnificPopup({
+      type:'inline',
+      midClick: true,
+    }
+    );
+});
 
 // input value
 $('.search-form-minus').on('click',function(event) {
-  event.preventDefault();
-  current = +$(this).siblings('input').val();
-  input = $(this).siblings('input');
-  $(input).val(current - 1);
+    event.preventDefault();
+    current = +$(this).siblings('input').val();
+    input = $(this).siblings('input');
+    $(input).val(current - 1);
 
-  if (current < 1) {
-    $(input).val(current);
-  }
-});
+    if (current < 1) {
+      $(input).val(current);
+    }
+  });
 
-$('.search-form-plus').on('click',function(event) {
-  event.preventDefault();
-  current = +$(this).siblings('input').val();
-  input = $(this).siblings('input');
-  $(input).val(current + 1);
-});
+  $('.search-form-plus').on('click',function(event) {
+    event.preventDefault();
+    current = +$(this).siblings('input').val();
+    input = $(this).siblings('input');
+    $(input).val(current + 1);
+  });
 
 // Disabling typing letters in numeric inputs
   $(":input[class='people-number']").keypress(function (e) { // selector by class is used for IE9 support
@@ -69,16 +68,12 @@ $('.search-form-plus').on('click',function(event) {
     }
   });
 
-});
-
-
-(function($){
 // range slider
 
 var slider = document.getElementById('slider');
 
 noUiSlider.create(slider, {
-  start: [0, 3000],
+start: [0, 3000],
   connect: true,
   step: 1,
   orientation: "horizontal",
@@ -92,7 +87,7 @@ noUiSlider.create(slider, {
 });
 
 var valueInputMin = document.getElementById('min-price'),
-valueInputMax = document.getElementById('max-price');
+    valueInputMax = document.getElementById('max-price');
 
 // When the slider value changes, update the input and span
 slider.noUiSlider.on('update', function( values, handle ) {
@@ -107,5 +102,3 @@ slider.noUiSlider.on('update', function( values, handle ) {
 valueInputMax.addEventListener('change', function(){
   slider.noUiSlider.set([null, this.value]);
 });
-
-})(jQuery);
