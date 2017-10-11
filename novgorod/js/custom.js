f55de74fbc0147bf635afb81b0562e692d3908b0
Veronika <v.b.171017@gmail.com>
2017-10-11 19:48:59 +0300

    upgrade

--- novgorod/js/custom.js
+++ novgorod/js/custom.js
@@ -103,3 +103,17 @@ $('.magnific-popup').magnificPopup({
 $('.parallax-window').parallax();
 
 });
+
+// Collapse accordion every time dropdown is shown
+$('.dropdown-accordion').on('show.bs.dropdown', function (event) {
+  var accordion = $(this).find($(this).data('accordion'));
+  accordion.find('.panel-collapse.in').collapse('hide');
+});
+
+// Prevent dropdown to be closed when we click on an accordion link
+$('.dropdown-accordion').on('click', 'a[data-toggle="collapse"]', function (event) {
+  event.preventDefault();
+  event.stopPropagation();
+  $($(this).data('parent')).find('.panel-collapse.in').collapse('hide');
+  $($(this).attr('href')).collapse('show');
+})
