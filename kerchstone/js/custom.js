8247fc9ef20ade00c69a5860c6afe7239a6eee61
Veronika <v.b.171017@gmail.com>
2018-03-17 15:57:25 +0300

    kerch

---
+++ kerchstone/js/custom.js
@@ -0,0 +1,45 @@
+$(function() {
+ 
+//*magnific-popup http://dimsemenov.com/plugins/magnific-popup/
+$('.magnific-popup').magnificPopup({
+	type: 'inline',
+	midClick: true,
+});
+
+// * Аякс отправка форм
+ //Документация: http://api.jquery.com/jquery.ajax/
+  $("#form").submit(function() {
+    $.ajax({
+      type: "POST",
+      url: "mail.php",
+      data: $(this).serialize()
+    }).done(function() {
+      $(this).find("input").val("");
+      alert("Спасибо за заявку!");
+      $("#form").trigger("reset");
+
+    });
+    return false;
+  });
+
+//wow.js
+
+var wow = new WOW(
+  {
+    boxClass:     'wow',      // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset:       200,          // distance to the element when triggering the animation (default is 0)
+    mobile:       false,       // trigger animations on mobile devices (default is true)
+    live:         true,       // act on asynchronously loaded content (default is true)
+    scrollContainer: null // optional scroll container selector, otherwise use window
+  }
+);
+wow.init();
+ 
+//parallax
+$('.parallax-window').parallax();
+})
+
+jQuery(function($){
+   $(".phone").mask("+7(999) 999-99-99");
+   });
