69660e749d9f69973cde9d0ed60a21df79160024
Veronika <v.b.171017@gmail.com>
2018-03-20 20:22:59 +0300

    new pages

--- kerchstone/js/custom.js
+++ kerchstone/js/custom.js
@@ -6,6 +6,21 @@ $('.magnific-popup').magnificPopup({
 	midClick: true,
 });
 
+$('.image-popup-no-margins').magnificPopup({
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
+		}
+	});
+
 // * Аякс отправка форм
  //Документация: http://api.jquery.com/jquery.ajax/
   $("#form").submit(function() {
