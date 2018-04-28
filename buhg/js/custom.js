e783e87bf451683a4b1b8dc819c59588f15a43f4
Veronika <v.b.171017@gmail.com>
2018-04-28 13:33:07 +0300

    update

--- buhg/js/custom.js
+++ buhg/js/custom.js
@@ -24,21 +24,7 @@ $('#js-carousel').slick({
 });
 
 
-// * Аякс отправка форм
- //Документация: http://api.jquery.com/jquery.ajax/
-  $("#form").submit(function() {
-    $.ajax({
-      type: "POST",
-      url: "mail.php",
-      data: $(this).serialize()
-    }).done(function() {
-      $(this).find("input").val("");
-      alert("Спасибо за заявку!");
-      $("#form").trigger("reset");
 
-    });
-    return false;
-  });
 
 
 /*WOW*/
@@ -58,8 +44,38 @@ $("body").niceScroll(
 	{
 		cursorwidth:"10px"
 	});
+//form validation
+$('#email').blur(function() {
+	if($(this).val() != '') {
+		var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+		if(pattern.test($(this).val())){
+			$(this).css({'border' : '1px solid #569b44'});
+			$('#valid').text('Верно');
+		} else {
+			$(this).css({'border' : '1px solid #ff0000'});
+			$('#valid').text('Не верно');
+		}
+	} else {
+		$(this).css({'border' : '1px solid #ff0000'});
+		$('#valid').text('Поле email не должно быть пустым');
+	}
+});
 
+// Form ajax
 
+$("#form").submit(function() {
+    $.ajax({
+      type: "POST",
+      url: "mail.php",
+      data: $(this).serialize()
+    }).done(function() {
+      $(this).find("input").val("");
+      alert("Спасибо за заявку!");
+      $("#form").trigger("reset");
+
+    });
+    return false;
+  });
 
 /* Smooth scroll / Scroll To Top
 /* ---------------------------------------------- */
--- buhg/mail.php
+++ buhg/mail.php
@@ -1,12 +1,40 @@
-<?php
+<?php 
 
-$recepient = "verino1377@mail.ru";
-$sitename  = "Sborka";
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
 
-$name = trim($_POST["name"]);
-$email = trim($_POST["email"]);
-$text = trim($_POST["text"]);
-$message = "Имя: $name \nEmail: $email \nТекст: $text";
+$name = $_POST['name'];
+$text = $_POST['text'];
+$email = $_POST['email'];
 
-$pagetitle = "Новая заявка с сайта \"$sitename\"";
-mail($recepient, $pagetitle,  $message,"Content-type: text/plain; charset=\"utf-8\" From: $recepient");
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.yandex.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'buh-expert-dnr@yandex.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'buh12101987'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('buh-expert-dnr@yandex.ru'); // от кого будет уходить письмо?
+$mail->addAddress('buh-expert-dnr@yandex.ru');     // Кому будет уходить письмо 
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Заявка с тестового сайта';
+$mail->Body    = '' .$name . ' оставил заявку, его email ' .$email. '<br>Его пожелания: ' .$text;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
+} else {
+     echo 'ok';
+}
+?>
